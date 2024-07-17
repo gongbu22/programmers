aabc091a69561d5a4dd64f8e37c64b9c063c11fb
Gongbu22 <99306831+gongbu22@users.noreply.github.com>
2024-07-17 14:16:25 +0900

    [level 0] Title: 코드 처리하기, Time: 15.79 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -0,0 +1,17 @@
+def solution(code):
+    mode = 0
+    ret = ''
+    
+    for i in range(len(code)):
+        if mode == 0:
+            if (code[i] != '1') & (i % 2 == 0):
+                ret += code[i]
+            elif code[i] == '1':
+                mode = 1
+        else:
+            if (code[i] != '1') & (i % 2 != 0):
+                ret += code[i]
+            elif code[i] == '1':
+                mode = 0
+    
+    return ret if len(ret) != 0 else "EMPTY"
