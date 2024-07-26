70124c3a0d08bfc0a0c33ae5ae4d5788e23d2763
Gongbu22 <99306831+gongbu22@users.noreply.github.com>
2024-07-26 13:25:40 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.py
@@ -0,0 +1,9 @@
+def solution(my_string, is_suffix):
+    answer = 0
+    for i in range(len(my_string)):
+        if my_string[i:] == is_suffix:
+            answer = 1
+            break
+        else:
+            answer = 0
+    return answer
