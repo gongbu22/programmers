92a0bcc80e220900320f196d0063ea682687c4a8
Gongbu22 <99306831+gongbu22@users.noreply.github.com>
2024-07-26 16:01:09 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.02 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.py
@@ -0,0 +1,9 @@
+def solution(my_string, is_prefix):
+    answer = 0
+    for i in range(len(my_string)):
+        if my_string[:i] == is_prefix:
+            answer = 1
+            break
+        else:
+            answer =0
+    return answer
