071b02f06925561d78e5dcd477ffe593cbc0f844
Gongbu22 <99306831+gongbu22@users.noreply.github.com>
2025-04-03 22:00:07 +0900

    [Bronze V] Title: 시험 성적, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,11 @@
+num = int(input())
+if (num>=90 and num<=100):
+    print('A')
+elif (num>=80 and num<90):
+    print('B')
+elif (num>=70 and num<80):
+    print('C')
+elif (num>=60 and num<70):
+    print('D')
+else:
+    print('F')
