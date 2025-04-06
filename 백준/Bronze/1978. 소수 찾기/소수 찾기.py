e274b728744cb04c72839956ff52ef377ac942e0
Gongbu22 <99306831+gongbu22@users.noreply.github.com>
2025-04-06 16:16:23 +0900

    [Bronze II] Title: 소수 찾기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,17 @@
+n = int(input())
+nums = map(int, input().split())
+
+count = 0
+
+def primenumber(num):
+    if num <= 1: return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if (num % i == 0):
+            return False
+    return True
+
+for i in nums:
+    if primenumber(i):
+        count += 1
+
+print(count)
